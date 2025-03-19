5550cf54816162749b63baeb0c4002ec1a07d12b
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-19 09:34:43 -0700

    update

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,21 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
-import storage from "redux-persist/lib/storage";
-import { persistReducer, persistStore } from "redux-persist";
+import {configureStore} from "@reduxjs/toolkit"
+import storage from "redux-persist/lib/storage"
+import {persistReducer, persistStore} from "redux-persist"
+
 const persistConfig = {
-  key: "users",
-  storage,
-};
+    key: "users",
+    storage,
+}
 
-const persistedUserReducer = persistReducer(persistConfig, userReducer);
+const persistedUserReducer = persistReducer(persistConfig, userReducer)
 
-export const store = configureStore({
-  reducer: {
-    users: persistedUserReducer,
-  },
-  devTools: process.env.NODE_ENV !== "production",
-});
+export const store = configureStore ({
+    reducer:{
+        users: persistedUserReducer,
+    }
 
-export const persistor = persistStore(store);
+})
 
-export type RootState = ReturnType<typeof store.getState>;
+export const persistor = persistStore(store)
+
+export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch;
