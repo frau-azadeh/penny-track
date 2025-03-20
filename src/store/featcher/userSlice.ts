a16fb24e9394afc5fff1db6e7471732496bf14b9
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-20 08:05:16 -0700

    update

--- src/store/featcher/userSlice.ts
+++ src/store/featcher/userSlice.ts
@@ -1,49 +1,43 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { v4 as uuidv4 } from "uuid";
+import {v4 as uuidv4} from "uuid"
 interface User {
   id: string;
   name: string;
   email: string;
 }
 
-interface UserState {
-  users: User[];
+interface UserState{
+  users: User[]
 }
 
 const initialState: UserState = {
   users: [],
-};
+}
 
-const userSlice = createSlice({
+export const userSlice = createSlice ({
   name: "users",
   initialState,
-  reducers: {
-    addUser: (
-      state,
-      action: PayloadAction<{ name: string; email: string }>,
-    ) => {
-      const nweUser: User = {
+  reducers:{
+    addUser:(state, action: PayloadAction<{name: string, email: string}>)=>{
+      const newUser : User = {
         id: uuidv4(),
         name: action.payload.name,
-        email: action.payload.email,
+        email: action.payload.email
       };
-      state.users.push(nweUser);
+      state.users.push(newUser)
     },
-    updateUser: (
-      state,
-      action: PayloadAction<{ id: string; name: string; email: string }>,
-    ) => {
+    updateUser:(state, action:PayloadAction<{id: string, name: string, email: string}>)=>{
       const user = state.users.find((user) => user.id === action.payload.id);
-      if (user) {
-        user.name = action.payload.name;
+      if(user){
         user.email = action.payload.email;
+        user.name = action.payload.name;
       }
     },
-    deleteUser: (state, action: PayloadAction<string>) => {
-      state.users = state.users.filter((user) => user.id !== action.payload);
-    },
-  },
-});
+    deleteUser:(state, action: PayloadAction<string>)=>{
+      state.users = state.users.filter((user)=>user.id !== action.payload)
+    }
+  }
+})
 
-export const { addUser, updateUser, deleteUser } = userSlice.actions;
-export default userSlice.reducer;
+export const {addUser, updateUser, deleteUser} = userSlice.actions;
+export default userSlice.reducer
