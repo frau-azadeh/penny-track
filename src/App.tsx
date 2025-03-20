aba6be019e9ec72d711c72c25c4e0fa3dc90e235
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-20 10:35:39 -0700

    update prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import {Button} from "./components/ui";
+import { Button } from "./components/ui";
 
 function App() {
   return (
     <div className="flex flex-col items-center m-2">
-      <h1 >Happy new year</h1>
+      <h1>Happy new year</h1>
       <Button variant="primary" onClick={() => alert("happy new year")}>
         click me!
       </Button>
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -15,7 +15,8 @@ const Button: React.FC<ButtonProps> = ({
   variant = "primary",
   disabled = false,
 }) => {
-  const baseStyles = "px-4 py-2 rounded-lg font-semibold transition duration-300";
+  const baseStyles =
+    "px-4 py-2 rounded-lg font-semibold transition duration-300";
   const variantStyles = {
     primary: "bg-blue-600 text-white hover:bg-blue-700",
     secondary: "bg-gray-300 text-gray-700 hover:bg-gray-400",
