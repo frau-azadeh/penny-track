28689b55bb0d32ba6a00585bd2cc387e6ff28aff
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-22 05:01:46 -0700

    update

--- src/store/featcher/userSlice.ts
+++ src/store/featcher/userSlice.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import {v4 as uuidv4} from "uuid"
-
+import { v4 as uuidv4 } from "uuid";
 interface User{
   id: string;
   name: string;
@@ -11,31 +10,31 @@ interface UserState {
   users: User[];
 }
 
-const initialState : UserState = {
+const initialState: UserState = {
   users: [],
 }
 
 export const userSlice = createSlice({
   name: "users",
   initialState,
-  reducers: {
-    addUser:(state, action: PayloadAction<{name: string; email:string}>)=>{
-      const newUser:User ={
+  reducers:{
+    addUser:(state, action:PayloadAction<{name: string; email: string}>)=>{
+      const newUser : User = {
         id: uuidv4(),
         name: action.payload.name,
         email: action.payload.email,
       }
       state.users.push(newUser)
     },
-    updateUser:(state, action: PayloadAction<{id: string; name: string; email:string}>)=>{
+    updateUser:(state, action:PayloadAction<{id: string; name: string; email: string}>)=>{
       const user = state.users.find((user)=> user.id === action.payload.id)
       if(user){
-        user.email= action.payload.email,
-        user.name= action.payload.name
+        user.email= action.payload.email;
+        user.name = action.payload.name;
       }
     },
     deleteUser:(state, action:PayloadAction<string>)=>{
-      state.users = state.users.filter((user)=> user.id !== action.payload)
+      state.users.filter((user)=> user.id !== action.payload)
     }
   }
 })
