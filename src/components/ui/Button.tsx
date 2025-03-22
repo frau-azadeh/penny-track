0d632ae319ef06f229c5e6a94c2ab546eff1f1d8
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-22 05:54:55 -0700

    update

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,32 +1,31 @@
-import React, { ButtonHTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes } from 'react'
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondary" | "danger";
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+  variant?: "primary" | "secondray" | "danger";
 }
 
-const Button: React.FC<ButtonProps> = ({
+const Button:React.FC<ButtonProps> = ({
   children,
-  type = "button",
+  type="button",
   variant = "primary",
   className,
   ...rest
 }) => {
-  const baseStyles =
-    "px-4 py-2 rounded-lg font-semibold transition duration-300";
+  const baseStyles = "px-4 py-2 rounded-lg font-bold transition duration-300";
   const variantStyles = {
     primary: "bg-blue-600 text-white hover:bg-blue-700",
-    secondary: "bg-gray-300 text-gray-700 hover:bg-gray-400",
+    secondray: "bg-gray-300 text-gray-700 hover:bg-gray-400",
     danger: "bg-red-600 text-white hover:bg-red-700",
-  };
 
+  }
   return (
     <button
-      className={`${baseStyles} ${variantStyles[variant]} ${className || ""}`}
+      className={`${baseStyles} ${variantStyles[variant]}${className || ""}`}
       {...rest}
     >
       {children}
     </button>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
