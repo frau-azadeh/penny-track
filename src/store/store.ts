5a6db984cf6ba0e7e3faa64bf1945c54eb1b27f8
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-22 12:14:11 -0700

    test 1

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,23 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import useReducer from "./featcher/userSlice";
+import { configureStore } from "@reduxjs/toolkit"
+import {persistStore, persistReducer} from "redux-persist"
+import storage from "redux-persist/lib/storage"
+import useReducer from "./featcher/userSlice"
 
 const persistConfig = {
   key: "users",
   storage,
-};
+}
 
-const persistedUserReducer = persistReducer(persistConfig, useReducer);
+const persistedUserReducer = persistReducer(persistConfig,useReducer)
 
 export const store = configureStore({
-  reducer: {
-    users: persistedUserReducer,
+  reducer:{
+    users: persistedUserReducer
   },
-  devTools: process.env.NODE_ENV !== "production",
-});
+  devTools: process.env.NODE_ENV !== "production"
+})
 
-export const persistor = persistStore(store);
+export const persistor = persistStore(store)
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
