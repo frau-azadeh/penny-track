ca30bbc2dded39d9b52d60e0c124e88ee0418c64
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-23 04:02:33 -0700

    add Input Component

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,20 +1,14 @@
 import { InputHTMLAttributes } from "react";
-import {
-  FieldError,
-  FieldValues,
-  Path,
-  UseFormRegister,
-} from "react-hook-form";
+import { FieldError, FieldValues, Path, UseFormRegister } from "react-hook-form";
 
-interface InputProps<T extends FieldValues>
-  extends InputHTMLAttributes<HTMLInputElement> {
+interface InputProps <T extends FieldValues> extends InputHTMLAttributes<HTMLInputElement>{
   label: string;
   name: Path<T>;
   register: UseFormRegister<T>;
-  error?: FieldError;
+  error?:FieldError;
 }
 
-const Input = <T extends FieldValues>({
+const Input = <T extends FieldValues> ({
   label,
   name,
   register,
@@ -29,15 +23,14 @@ const Input = <T extends FieldValues>({
         id={name as string}
         {...register(name as Path<T>)}
         {...rest}
-        className={`px-4 py-2 rounded-lg focus: outline-none focus: ring-2 ${
+        className={`px-4 py-2 rounded-lg focus:outline-none focus:ring-2 ${
           error
-            ? "border-red-500 focus: ring-red-500"
-            : "border-gray-300 focus: ring-blue-500"
-        }${className || ""}`}
+          ? "border-red-500 focus: ring-red-500"
+          : "border-gray-300 focus:ring-blue-500"
+        } ${className || ""}`}
       />
       {error && <p className="text-red-500 text-sm">{error.message}</p>}
     </div>
-  );
-};
-
+  )
+}
 export default Input;
