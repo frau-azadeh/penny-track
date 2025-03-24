53e7594d6b0a80fad2c3994d3c63dfde79260676
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-24 10:18:08 -0700

    refactor persistor

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistReducer } from "redux-persist";
+import { persistReducer, persistStore } from "redux-persist";
 import useReducer from "./featcher/userSlice";
 import storage from "redux-persist/lib/storage";
 
@@ -16,6 +16,8 @@ export const store = configureStore({
   },
   devTools: process.env.NODE_ENV !== "production",
 });
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
