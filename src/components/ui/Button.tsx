4403d47269f6cd568bb1f8c92567d92b9963f218
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-24 04:41:46 -0700

    solve mistake typing

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,31 +1,32 @@
-import React, { ButtonHTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes} from 'react'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondray" | "danger";
+  variant?:"primary" | "secondray" | "danger"
+
 }
 
-const Button: React.FC<ButtonProps> = ({
+const Button:React.FC<ButtonProps> = ({
   children,
-  type = "button",
-  variant = "primary",
   className,
+  type="button",
+  variant="primary",
   ...rest
 }) => {
-  const baseStyles = "px-4 py-2 rounded-lg transion duration-300";
+  const baseStyles = "px-4 py-2 rounded-lg transition duration-300";
   const variantStyles = {
     primary: "bg-blue-600 text-white hover:bg-blue-900",
-    secondray: "bg-gray-300 test-gray-700 hover:bg-gray-900",
-    danger: "bg-red-600 text-white hover:bg-red-900",
-  };
+    secondray: "bg-gray-300 text-gray-700 hover:bg-gray-900",
+    danger: "bg-red-600 test-white hover:bg-red-900",
+  }
 
   return (
     <button
-      className={`${baseStyles} ${variantStyles[variant]} ${className || ""}`}
+      className={`${baseStyles} ${variantStyles[variant]}${className|| ""}`}
       {...rest}
     >
       {children}
     </button>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
