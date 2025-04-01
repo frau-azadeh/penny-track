fcf4e2785f35c6724497e08fcdd6586c14c9c076
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-01 10:06:43 -0700

    update prettier

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,26 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {persistStore, persistReducer} from "redux-persist"
-import useReducer from "./featcher/userSlice"
-import storage from "redux-persist/lib/storage"
+import { persistStore, persistReducer } from "redux-persist";
+import useReducer from "./featcher/userSlice";
+import storage from "redux-persist/lib/storage";
 const persistConfig = {
-  key : "users",
+  key: "users",
   storage,
-}
+};
 
-const persistedUserReducer = persistReducer(persistConfig, useReducer)
+const persistedUserReducer = persistReducer(persistConfig, useReducer);
 
 export const store = configureStore({
-  reducer:{
-    users:persistedUserReducer
+  reducer: {
+    users: persistedUserReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
-  devTools: process.env.NODE_ENV !== "production"
-})
+  devTools: process.env.NODE_ENV !== "production",
+});
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
