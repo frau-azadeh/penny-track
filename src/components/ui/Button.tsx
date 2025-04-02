cbdb8c42dcc0d1dd048aa18bebd1456a434e1dbb
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-02 10:20:14 -0700

    update hover css

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -13,9 +13,9 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
   const baseStyle = "px-4 py-2 rounded-lg transition duration-300";
   const variantStyle = {
-    primary: "bg-blue-600 text-white hover: bg-blue-900",
-    secondray: "bg-gray-600 text-white hover: bg-gray-900",
-    danger: "bg-red-600 text-white hover: bg-red-900",
+    primary: "bg-blue-600 text-white hover:bg-blue-900",
+    secondray: "bg-gray-600 text-white hover:bg-gray-900",
+    danger: "bg-red-600 text-white hover:bg-red-900",
   };
   return (
     <button
