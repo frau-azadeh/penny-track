24a3a2e7f93ce3bea8595612d6adeb4d991cc621
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-02 10:00:48 -0700

    update userSlice

--- src/store/featcher/userSlice.ts
+++ src/store/featcher/userSlice.ts
@@ -1,50 +1,45 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit"
 import { v4 as uuidv4 } from "uuid";
 
-interface User {
+interface User{
   id: string;
   name: string;
   email: string;
 }
 
 interface UserState {
-  users: User[];
+  users: User[]
 }
 
-const initialState: UserState = {
-  users: [],
-};
+const initialState: UserState= {
+  users:[]
+}
 
 export const userSlice = createSlice({
-  name: "users",
-  initialState,
-  reducers: {
-    addUser: (
-      state,
-      action: PayloadAction<{ name: string; email: string }>,
-    ) => {
-      const newUser: User = {
-        id: uuidv4(),
-        name: action.payload.name,
-        email: action.payload.email,
-      };
-      state.users.push(newUser);
-    },
-    updateUser: (
-      state,
-      action: PayloadAction<{ id: string; name: string; email: string }>,
-    ) => {
-      const user = state.users.find((user) => user.id === action.payload.id);
-      if (user) {
-        user.email = action.payload.email;
-        user.name = action.payload.name;
-      }
-    },
-    deleteUser: (state, action: PayloadAction<string>) => {
-      state.users = state.users.filter((user) => user.id !== action.payload);
-    },
+name: "users",
+initialState,
+reducers:{
+  addUser: (state, action: PayloadAction<{name: string; email: string}>)=>{
+    const newUser: User ={
+      id: uuidv4(),
+      name: action.payload.name,
+      email: action.payload.email,
+    }
+    state.users.push(newUser)
   },
-});
+  updateUser: (state, action: PayloadAction<{id: string; name: string; email:string}>)=>{
+    const user = state.users.find((user) => user.id === action.payload.id);
+    if(user){
+      user.email= action.payload.email;
+      user.name= action.payload.name;
+    }
+  },
+  deleteUser: (state, action:PayloadAction<string>)=>{
+    state.users = state.users.filter((user)=> user.id !== action.payload)
+  }
+}
+})
 
-export const { addUser, updateUser, deleteUser } = userSlice.actions;
+export const {addUser, deleteUser, updateUser} = userSlice.actions
 export default userSlice.reducer;
+
