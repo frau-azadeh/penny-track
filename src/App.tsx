9a1de5eae54a0de1903eeb86a1887a9d73c93405
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-04 08:27:57 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { UserList, UserManager } from "./components/lists";
 import ExpenseManager from "./components/managers/ExpenseManager";
 import ProductDashboard from "./components/managment/ProductDashboard";
 import ProductManager from "./components/managment/ProductManager";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="flex flex-col items-center m-2">
+      <ToastContainer position="top-center" />
       <h1>Happy new year</h1>
       <UserManager />
       <UserList />
