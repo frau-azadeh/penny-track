c9d1ad4af6dd540186deac751652bce5dd28ecf1
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-04 05:29:00 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import { UserManager } from "./components/lists";
+import { UserList, UserManager } from "./components/lists";
 
 function App() {
   return (
     <div className="flex flex-col items-center m-2">
       <h1>Happy new year</h1>
       <UserManager />
+      <UserList/>
     </div>
   );
 }
