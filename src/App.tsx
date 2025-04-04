89e193f322a3596d7e5377f038cef67f9ca7aeee
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-04 05:29:10 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { UserList, UserManager } from "./components/lists";
+import ExpenseManager from "./components/managers/ExpenseManager";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <h1>Happy new year</h1>
       <UserManager />
       <UserList/>
+      <ExpenseManager/>
     </div>
   );
 }
