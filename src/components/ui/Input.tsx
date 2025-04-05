0ba63ad9f80397ef90e7ddf33d3eef7642bee7b3
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-05 06:53:53 -0700

    update

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,43 +1,27 @@
-import { InputHTMLAttributes } from "react";
-import {
-  FieldError,
-  FieldValues,
-  UseFormRegister,
-  Path,
-} from "react-hook-form";
+import  { InputHTMLAttributes, forwardRef } from "react";
+import { FieldError } from "react-hook-form";
 
-interface InputProps<T extends FieldValues>
-  extends InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   error?: FieldError;
-  register?: UseFormRegister<T>; // اصلاح شد
-  name: Path<T> | string; // اصلاح شد
 }
 
-const Input = <T extends FieldValues>({
-  label,
-  name,
-  error,
-  className,
-  register,
-  ...rest
-}: InputProps<T>) => {
-  return (
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, name, error, className, ...rest }, ref) => (
     <div>
-      <label htmlFor={name as string}>{label}</label>
+      <label htmlFor={name}>{label}</label>
       <input
-        id={name as string}
-        {...(register ? register(name as Path<T>) : {})} // تغییر در استفاده از register
-        {...rest}
+        id={name}
+        name={name}
+        ref={ref}
         className={`px-4 py-2 rounded-lg focus:outline-none focus:ring-2 ${
-          error
-            ? "border-red-500 focus:ring-red-500"
-            : "border-gray-300 focus:ring-blue-500"
+          error ? "border-red-500 focus:ring-red-500" : "border-gray-300 focus:ring-blue-500"
         } ${className || ""}`}
+        {...rest}
       />
       {error && <p className="text-red-500 text-sm">{error.message}</p>}
     </div>
-  );
-};
+  )
+);
 
 export default Input;
