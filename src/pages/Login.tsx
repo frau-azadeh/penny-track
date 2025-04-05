cc3b414961296fc6fbee818659e46e801eafba2c
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-04 21:08:52 -0700

    add Login Page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginForm from "../components/forms/LoginForm";
+
+const Login: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="w-full max-w-md">
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
