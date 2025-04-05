67543766cb05fafe1e780e70f40040fc103866c6
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-05 01:26:20 -0700

    update

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -4,17 +4,37 @@ import ProductDashboard from "../components/managment/ProductDashboard";
 import ProductManager from "../components/managment/ProductManager";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
 
 function Dashboard() {
+  const role: "admin" | "user" | null = useSelector(
+    (state: RootState) => state.auth.role
+  );
+
   return (
     <div className="flex flex-col items-center m-2">
       <ToastContainer position="top-center" />
-      <h1>my dashboard</h1>
-      <UserManager />
-      <UserList />
-      <ExpenseManager />
-      <ProductDashboard />
-      <ProductManager />
+      <h1>My Dashboard</h1>
+
+      {role === "admin" ? (
+        <>
+          <h2>Admin Panel</h2>
+          <UserManager />
+          <UserList />
+          <ExpenseManager />
+          <ProductDashboard />
+          <ProductManager />
+        </>
+      ) : role === "user" ? (
+        <>
+          <h2>User Panel</h2>
+          <ProductDashboard />
+          <ExpenseManager />
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
