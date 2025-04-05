14d54ea0c6ae8ed8b0aa52a0798af1e3cc45428f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-05 01:26:52 -0700

    update

---
+++ src/store/middleware/authMiddleware.ts
@@ -0,0 +1,27 @@
+import { Middleware, isAnyOf } from "@reduxjs/toolkit";
+import {jwtDecode} from "jwt-decode";  // ✅ استفاده از jwt-decode
+import { logout, login } from "../featcher/authSlice";
+import { toast } from "react-toastify";
+
+const authMiddleware: Middleware = (store) => (next) => (action) => {
+  // بررسی اینکه آیا اکشن مورد نظر ماست
+  if (isAnyOf(login)(action)) {
+    const token = action.payload;
+
+    try {
+      // دیکد کردن توکن با استفاده از jwt-decode
+      const decoded = jwtDecode(token);
+      console.log("Token Decoded:", decoded);
+      return next(action); // ادامه پردازش اکشن پس از تایید
+    } catch (error) {
+      console.error("Token decoding failed:", error);
+      toast.error("Invalid token, logging out.");
+      store.dispatch(logout());
+      return;
+    }
+  }
+
+  return next(action);
+};
+
+export default authMiddleware;
