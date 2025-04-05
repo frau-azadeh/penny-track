c292efe3eace7c0982839d23d4f807811f590c2d
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-04 21:06:41 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import "./App.css";
-import { UserList, UserManager } from "./components/lists";
-import ExpenseManager from "./components/managers/ExpenseManager";
-import ProductDashboard from "./components/managment/ProductDashboard";
-import ProductManager from "./components/managment/ProductManager";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { store, persistor } from "./store/store";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
   return (
-    <div className="flex flex-col items-center m-2">
-      <ToastContainer position="top-center" />
-      <h1>Happy new year</h1>
-      <UserManager />
-      <UserList />
-      <ExpenseManager />
-      <ProductDashboard />
-      <ProductManager />
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <ToastContainer position="top-center" />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          </Routes>
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
