acdc513f2a29bda90cb058b6a64eaed711c1d08a
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-06 03:01:55 -0700

    refactor Input component

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,33 +1,37 @@
-import { forwardRef, InputHTMLAttributes } from "react";
+import { forwardRef, InputHTMLAttributes } from "react"
 import { FieldError } from "react-hook-form";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
   label: string;
-  error?: FieldError;
+  error: FieldError;
 }
+const Input =forwardRef<HTMLInputElement, InputProps> (({
+  label,
+  name,
+  className,
+  error,
+  ...rest
 
-const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ label, name, error, className, ...rest }, ref) => {
-    return (
-      <div>
-        <label htmlFor={name}>{label}</label>
-        <input
-          id={name}
-          name={name}
-          ref={ref}
-          className={`px-4 py-2 rounded-lg focus:outline-none focus:ring ${
-            error
-              ? "border-red-500 focus:ring-red-500"
+},ref ) => {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input
+        id={name}
+        name={name}
+        ref={ref}
+        className={`px-4 py-2 rounded-lg focus:outline-none focus:ring${
+          error
+              ? "border-red-600 focus:ring-red-500"
               : "border-gray-300 focus:ring-blue-500"
-          } ${className || ""}`}
-          {...rest}
-        />
-        {error && <p className="text-red-500 text-sm">{error.message}</p>}
-      </div>
-    );
-  },
-);
-
+        }`}
+        {...rest}
+      />
+      {error && <p className="text-red-500">{error.message}</p>}
+    </div>
+  )
+}
+)
 Input.displayName = "Input";
 
-export default Input;
+export default Input
