97be104a320ffdbd8ed488dbd2f84c1f162ab0b3
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-06 03:28:23 -0700

    update modal

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,34 +1,33 @@
-import React, { ReactNode } from "react";
-import { createPortal } from "react-dom";
-import Button from "./Button";
+import React, { ReactNode } from 'react'
+import { createPortal } from 'react-dom';
+import Button from './Button';
 
-interface ModalProps {
+interface ModalProps{
   isOpen: boolean;
-  onClose: () => void;
-  children: ReactNode;
+  onClose: ()=> void;
+  children: ReactNode
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
-  if (!isOpen) return null;
-  return createPortal(
-    <div className="fixed inset-0 z-50 flex items-center justy bg-black/50">
-      <div className="absolute inset-0" onClick={onClose} />
-      <div
-        className="relative z-10 bg-white rounded-xl shadow-xl w-full max-w-md mx-4"
-        onClick={(e) => e.stopPropagation()}
-      >
-        <Button
-          onClick={onClose}
-          variant="secondary"
-          className="absolute top-3 px-22 py-1 text-xl leading-none"
+const Modal:React.FC<ModalProps> = ({isOpen, onClose, children}) => {
+  if(!isOpen) return null
+  return createPortal (
+    <div className='fiexed bg-black/40 inset-0 z-50 flex items-center justify-center'>
+      <div onClick={onClose} className='inset-0 absolute'/>
+        <div className='relative z-10 bg-white rounded-xl shadow-xl w-full max-w-md mx-4'
+          onClick={(e)=>e.stopPropagation()}
         >
-          &times;
-        </Button>
-        {children}
-      </div>
+          <Button
+            onClick={onClose}
+            variant='secondary'
+            className='absoulute top-3 px-22 py-1 leading-none text-xl'
+          >
+            &times;
+          </Button>
+          {children}
+        </div>
     </div>,
     document.body,
-  );
-};
+  )
+}
 
-export default Modal;
+export default Modal
