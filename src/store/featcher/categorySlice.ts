2ad1ea611a6262a832d6914bd59f31ecbd9439c7
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-07 03:48:13 -0700

    update to fa

--- src/store/featcher/categorySlice.ts
+++ src/store/featcher/categorySlice.ts
@@ -1,27 +1,27 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-export interface CategoryState {
+export interface CategoryState{
   categories: string[];
   selectedCategory: string;
 }
 
-const initialState: CategoryState = {
-  categories: ["All", "Electronics", "Clothing", "Books", "Food", "Things"],
-  selectedCategory: "All",
-};
+const initialState: CategoryState={
+  categories: ["همه","غذا","میوه","کتاب","لباس"],
+  selectedCategory: "همه",
+}
 
 const categorySlice = createSlice({
   name: "category",
   initialState,
-  reducers: {
-    setCategories: (state, action: PayloadAction<string[]>) => {
-      state.categories = action.payload;
+  reducers:{
+    setCategories: (state, action:PayloadAction<string[]>)=>{
+      state.categories = action.payload
     },
-    selectCategory: (state, action: PayloadAction<string>) => {
-      state.selectedCategory = action.payload;
-    },
-  },
-});
+    selectCategory: (state, action: PayloadAction<string>)=>{
+      state.selectedCategory = action.payload
+    }
+  }
+})
 
-export const { setCategories, selectCategory } = categorySlice.actions;
+export const {selectCategory, setCategories} = categorySlice.actions
 export default categorySlice.reducer;
