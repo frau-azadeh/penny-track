473237657df3086d1819ab1b26ac986202f3a011
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-08 09:27:24 -0700

    update categorySlice with export

--- src/store/featcher/categorySlice.ts
+++ src/store/featcher/categorySlice.ts
@@ -8,20 +8,20 @@ export interface CategoryState {
 const initialState: CategoryState = {
   categories: ["همه", "غذا", "میوه", "کتاب", "لباس", "سایر"],
   selectedCategory: "همه",
-}
+};
 
 export const categorySlice = createSlice({
   name: "category",
   initialState,
   reducers: {
-    setCategories: (state, action: PayloadAction<string[]>)=>{
-      state.categories = action.payload
+    setCategories: (state, action: PayloadAction<string[]>) => {
+      state.categories = action.payload;
     },
-    selectCategory: (state, action: PayloadAction<string>)=>{
-      state.selectedCategory = action.payload
-    }
-  }
-})
+    selectCategory: (state, action: PayloadAction<string>) => {
+      state.selectedCategory = action.payload;
+    },
+  },
+});
 
-export const {setCategories, selectCategory} = categorySlice.actions;
+export const { setCategories, selectCategory } = categorySlice.actions;
 export default categorySlice.reducer;
