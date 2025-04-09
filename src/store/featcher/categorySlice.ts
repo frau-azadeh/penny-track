098aa910cd62952c272bb8e3631f17824f8d17ed
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-09 02:22:12 -0700

    update prettier

--- src/store/featcher/categorySlice.ts
+++ src/store/featcher/categorySlice.ts
@@ -1,26 +1,26 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-interface CategoryState{
+interface CategoryState {
   categories: string[];
-  selectedCategory: string
+  selectedCategory: string;
 }
 
-const initialState : CategoryState = {
-  categories :["همه","غذا","میوه","کتاب","لباس","سایر"],
+const initialState: CategoryState = {
+  categories: ["همه", "غذا", "میوه", "کتاب", "لباس", "سایر"],
   selectedCategory: "همه",
-}
+};
 export const categorySlice = createSlice({
   name: "category",
   initialState,
   reducers: {
-    setCategories: (state, action: PayloadAction<string[]>) =>{
-      state.categories = action.payload
+    setCategories: (state, action: PayloadAction<string[]>) => {
+      state.categories = action.payload;
     },
-    selectCategory: (state, action: PayloadAction<string>) =>{
-      state.selectedCategory = action.payload
-    }
-  }
-})
+    selectCategory: (state, action: PayloadAction<string>) => {
+      state.selectedCategory = action.payload;
+    },
+  },
+});
 
-export const {setCategories, selectCategory} = categorySlice.actions
-export default categorySlice.reducer
+export const { setCategories, selectCategory } = categorySlice.actions;
+export default categorySlice.reducer;
