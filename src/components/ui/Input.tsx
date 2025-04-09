99ebdc9f4cd29129a28641a32b652fb6262f2b75
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-09 03:34:03 -0700

    update prettier

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,32 +1,29 @@
-import { forwardRef, InputHTMLAttributes } from 'react'
-import { FieldError } from 'react-hook-form';
+import { forwardRef, InputHTMLAttributes } from "react";
+import { FieldError } from "react-hook-form";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
-  error?: FieldError; 
+  error?: FieldError;
 }
 
-const Input =forwardRef<HTMLInputElement, InputProps> (
-  (
-    {label, name, error, className, ...rest}, ref
-  ) => {
-  return (
-    <div>
-      <label htmlFor={name}>{label}</label>
-      <input
-        id={name}
-        name={name}
-        ref={ref}
-        className={` px-4 py-2 rounded-lg focus:outline-none focus:ring-2 ${
-          error 
-            ? "border border-red-500 focus:ring-red-500"
-            : "border border-gray-500 focus:ring-gray-500"
-
-        }`}
-        {...rest}
-      />
-    </div>
-  )
-}
-)
-export default Input
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, name, error, className, ...rest }, ref) => {
+    return (
+      <div>
+        <label htmlFor={name}>{label}</label>
+        <input
+          id={name}
+          name={name}
+          ref={ref}
+          className={` px-4 py-2 rounded-lg focus:outline-none focus:ring-2 ${
+            error
+              ? "border border-red-500 focus:ring-red-500"
+              : "border border-gray-500 focus:ring-gray-500"
+          }`}
+          {...rest}
+        />
+      </div>
+    );
+  },
+);
+export default Input;
