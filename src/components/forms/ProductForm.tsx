3484cf1c6e98f7b2ba1f1b1fbef92ffdb8003f72
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-10 11:12:28 -0700

    refactor

--- src/components/forms/ProductForm.tsx
+++ src/components/forms/ProductForm.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { Button, Input } from "../ui";
+import React from 'react'
+import { Button, Input } from '../ui';
+import {useForm} from "react-hook-form"
 
 export interface ProductFormValues {
   name: string;
@@ -20,69 +20,74 @@ const ProductForm: React.FC<ProductFormProps> = ({
   defaultValues,
   onSubmit,
 }) => {
-  const {
+  const{
     register,
     handleSubmit,
     reset,
-    formState: { errors },
+    formState: {errors},
   } = useForm<ProductFormValues>({
     defaultValues,
   });
 
-  const handleFormSubmit = (data: ProductFormValues) => {
+  const handleFormSubmit = (data: ProductFormValues) =>{
     onSubmit(data);
     reset();
   };
+
   return (
-    <form onSubmit={handleSubmit(handleFormSubmit)} className="space-y-4 p-4">
-      <h3 className="text-xl font-bold m-4">افزودن / ویرایش محصول</h3>
-      <div className="grid grid-col-1 gap-4">
+    <form onSubmit={handleSubmit(handleFormSubmit)} className='space-y-4 p-4'>
+      <h3 className='text-xl font-bold m-4'>افزودن / ویرایش محصول</h3>
+      <div className='grid grid-col-1 gap-4'>
         <Input
-          label="نام محصول"
-          {...register("name", { required: "نام محصول الزامی است" })}
-          placeholder="نام محصول"
+          label='نام محصول'
+          placeholder='نام محصول'
+          type='text'
           error={errors.name}
+          {...register("name",{required: "نام محصول الزامی است"})}
         />
         <Input
-          label="دسته بندی"
-          {...register("category", { required: "دسته بندی الزامی میباشد" })}
-          placeholder="دسته بندی"
+          label='دسته بندی'
+          placeholder='دسته  بندی'
+          type='text'
           error={errors.category}
+          {...register("category", {required: "دسته بندی الزامی است"})}
         />
-        <Input
-          label="توضیحات"
-          {...register("description", { required: "توضیحات الزامی است" })}
-          placeholder="توضیحات"
+        <Input 
+          label='توضیحات'
+          placeholder='توضیحات'
+          type='text'
           error={errors.description}
+          {...register("description", {required: "توضیحات الزامی است"})}
         />
         <Input
-          label="قیمت"
-          type="number"
-          {...register("price", { required: "قیمت الزامی است" })}
-          placeholder="قیمت"
+          label='قیمت'
+          placeholder='قیمت'
           error={errors.price}
+          type='number'
+          {...register("price", {required: "قیمت الزامی است"})}
         />
         <Input
-          label="تعداد"
-          type="number"
-          {...register("quantity", { required: "تعداد الزامی است" })}
-          placeholder="تعداد"
+          label='تعداد'
+          placeholder='تعداد'
+          type='number'
           error={errors.quantity}
+          {...register("quantity", {required: "تعداد الزامی است"})}
         />
         <Input
-          label="تاریخ"
-          type="date"
-          {...register("date", { required: "تاریح الزامی است" })}
+          label='تاریخ'
+          placeholder='تاریخ'
+          type='date'
           error={errors.date}
+          {...register("date", {required: "تاریخ الزامی است"})}
         />
       </div>
-      <div className="flex justify-end w-full ">
-        <Button type="submit" variant="primary" className="px-4 py-2">
+      <div className='flex justify-end mt-4'>
+        <Button type='submit' variant='primary' className='px-4 py-2'>
           ذخیره
         </Button>
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default ProductForm;
+export default ProductForm
