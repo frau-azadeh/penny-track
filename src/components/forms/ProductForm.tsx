eb783993e10374c794b81bc942c7c18338d27b4f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-12 01:24:17 -0700

    update prettier

--- src/components/forms/ProductForm.tsx
+++ src/components/forms/ProductForm.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { useForm } from 'react-hook-form';
-import { Button, Input } from '../ui';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Button, Input } from "../ui";
 
-export interface ProductFormValues{
+export interface ProductFormValues {
   name: string;
   description: string;
   category: string;
@@ -11,7 +11,7 @@ export interface ProductFormValues{
   date: string;
 }
 
-interface ProductFormProps{
+interface ProductFormProps {
   defaultValues?: ProductFormValues;
   onSubmit: (data: ProductFormValues) => void;
 }
@@ -20,69 +20,69 @@ const ProductForm: React.FC<ProductFormProps> = ({
   defaultValues,
   onSubmit,
 }) => {
-  const{
+  const {
     register,
     handleSubmit,
     reset,
-    formState: {errors},
+    formState: { errors },
   } = useForm<ProductFormValues>({
     defaultValues,
   });
-  const handleFormSubmit = (data: ProductFormValues)=>{
+  const handleFormSubmit = (data: ProductFormValues) => {
     onSubmit(data);
     reset();
-  }
+  };
   return (
-    <form onSubmit={handleSubmit(handleFormSubmit)} className='space-y-4 p-4'>
-      <h3 className='text-xl font-bold m-4'>افزودن / ویرایش محصول</h3>
-      <div className='grid grid-col-1 gap-4'>
+    <form onSubmit={handleSubmit(handleFormSubmit)} className="space-y-4 p-4">
+      <h3 className="text-xl font-bold m-4">افزودن / ویرایش محصول</h3>
+      <div className="grid grid-col-1 gap-4">
         <Input
-          label='نام محصول'
-          placeholder='نام محصول'
-          {...register("name",{required: "نام محصول الزامی است."})}
+          label="نام محصول"
+          placeholder="نام محصول"
+          {...register("name", { required: "نام محصول الزامی است." })}
           error={errors.name}
         />
         <Input
-          label='دسته بندی'
-          placeholder='دسته بندی'
-          {...register("category", {required: "دسته بندی الزامی است"})}
+          label="دسته بندی"
+          placeholder="دسته بندی"
+          {...register("category", { required: "دسته بندی الزامی است" })}
           error={errors.category}
         />
         <Input
-          label='توضیحات'
-          placeholder='توضیحات'
-          {...register("description", {required: "توضیحات الزامی است"})}
+          label="توضیحات"
+          placeholder="توضیحات"
+          {...register("description", { required: "توضیحات الزامی است" })}
           error={errors.description}
         />
         <Input
-          label='قیمت'
-          placeholder='قیمت'
-          type='number'
-          {...register("price", {required: "قیمت الزامی است"})}
+          label="قیمت"
+          placeholder="قیمت"
+          type="number"
+          {...register("price", { required: "قیمت الزامی است" })}
           error={errors.price}
         />
         <Input
-          label='تعداد'
-          placeholder='تعداد'
-          type='number'
+          label="تعداد"
+          placeholder="تعداد"
+          type="number"
           error={errors.quantity}
-          {...register("quantity", {required: "تعداد الزامی است"})}
+          {...register("quantity", { required: "تعداد الزامی است" })}
         />
         <Input
           label="تاریخ"
-          placeholder='تاریخ'
+          placeholder="تاریخ"
           type="date"
           error={errors.date}
-          {...register("date", {required: "تاریخ الزامی است"})}
+          {...register("date", { required: "تاریخ الزامی است" })}
         />
       </div>
-      <div className='flex justify-center m-4'>
-        <Button variant='primary' type='submit' className='px-4 py-2'>
+      <div className="flex justify-center m-4">
+        <Button variant="primary" type="submit" className="px-4 py-2">
           ذخیره
         </Button>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default ProductForm
+export default ProductForm;
