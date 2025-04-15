855bf11c61c2e681d2edb1682692d542b3f1018d
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-15 04:00:12 -0700

    update prettier

--- src/components/filters/ExpenseSearch.tsx
+++ src/components/filters/ExpenseSearch.tsx
@@ -1,35 +1,34 @@
-import React, { useState, useTransition } from 'react'
-import { setSearchQuery } from '../../store/featcher/expenseSlice';
-import { Input } from '../ui';
-import { useDispatch } from 'react-redux';
+import React, { useState, useTransition } from "react";
+import { setSearchQuery } from "../../store/featcher/expenseSlice";
+import { Input } from "../ui";
+import { useDispatch } from "react-redux";
 
-const ExpenseSearch:React.FC = () => {
+const ExpenseSearch: React.FC = () => {
   const [query, setQuery] = useState("");
   const dispatch = useDispatch();
   const [isPending, startTransition] = useTransition();
 
-const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) =>{
-  const value = event.target.value;
-  setQuery(value);
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setQuery(value);
 
-  startTransition(()=>{
-    dispatch(setSearchQuery(value));
-  });
-};
-
+    startTransition(() => {
+      dispatch(setSearchQuery(value));
+    });
+  };
 
   return (
-    <div className='mb-4'>
+    <div className="mb-4">
       <Input
-        label='جستجو'
+        label="جستجو"
         value={query}
         onChange={handleSearch}
-        placeholder='نام یا متن مورد نظر را برای جستجو وارد کنید'
-        className='w-full'
+        placeholder="نام یا متن مورد نظر را برای جستجو وارد کنید"
+        className="w-full"
       />
-      {isPending && <p className='text-gray-500'>به روزرسانی نتیجه ... </p>}
+      {isPending && <p className="text-gray-500">به روزرسانی نتیجه ... </p>}
     </div>
-  )
-}
+  );
+};
 
-export default ExpenseSearch
+export default ExpenseSearch;
