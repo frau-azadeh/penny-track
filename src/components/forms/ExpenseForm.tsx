daff2cec845e1b890f2d42595b6d8972656e8619
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-15 06:27:49 -0700

    update prettier

--- src/components/forms/ExpenseForm.tsx
+++ src/components/forms/ExpenseForm.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { Button, Input } from '../ui';
+import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Button, Input } from "../ui";
 
-export interface ExpenseFormValues{
+export interface ExpenseFormValues {
   title: string;
   amount: number;
   date: string;
   description: string;
 }
 
-interface ExpenseFormProps{
+interface ExpenseFormProps {
   onSubmit: (data: ExpenseFormValues) => void;
   defaultValues?: ExpenseFormValues;
 }
@@ -18,69 +18,70 @@ const ExpenseForm: React.FC<ExpenseFormProps> = ({
   onSubmit,
   defaultValues,
 }) => {
-
   const {
     reset,
     handleSubmit,
     register,
-    formState: {errors},
-
+    formState: { errors },
   } = useForm<ExpenseFormValues>({
     defaultValues,
-  })
+  });
 
-  const submitHandler: SubmitHandler<ExpenseFormValues> = (data) =>{
+  const submitHandler: SubmitHandler<ExpenseFormValues> = (data) => {
     data.amount = Number(data.amount);
     onSubmit(data);
     reset();
-  }
+  };
 
   return (
-    <form 
+    <form
       onSubmit={handleSubmit(submitHandler)}
-      className='space-y-4 bg-white p-6 rounded-lg shadow-md'
+      className="space-y-4 bg-white p-6 rounded-lg shadow-md"
     >
-      <h2 className='text-lg font-bold text-gray-700'>{defaultValues ? "ویرایش هزینه" : "افزودن هزینه"}</h2>
+      <h2 className="text-lg font-bold text-gray-700">
+        {defaultValues ? "ویرایش هزینه" : "افزودن هزینه"}
+      </h2>
       <Input
-        label='عنوان'
-        {...register("title",{required: "عنوان الزامی است"})}
+        label="عنوان"
+        {...register("title", { required: "عنوان الزامی است" })}
         error={errors.title}
-        placeholder='عنوان هزینه را وارد کنید'
-        className='w-full'
+        placeholder="عنوان هزینه را وارد کنید"
+        className="w-full"
       />
 
-      <Input 
-        label='مبلغ'
-        {...register("amount",{required: "مبلغ الزامی است",
+      <Input
+        label="مبلغ"
+        {...register("amount", {
+          required: "مبلغ الزامی است",
           valueAsNumber: true,
-          min: { value: 1, message: "مبلغ باید بزرگتر از 0 باشد"}
+          min: { value: 1, message: "مبلغ باید بزرگتر از 0 باشد" },
         })}
-        type='number'
+        type="number"
         error={errors.amount}
-        placeholder='مبلغ را وارد کنید'
-        className='w-full'
+        placeholder="مبلغ را وارد کنید"
+        className="w-full"
       />
 
       <Input
-        label='تاریخ'
-        {...register("date", {required: "تاریخ الزامی است"})}
-        type='date'
+        label="تاریخ"
+        {...register("date", { required: "تاریخ الزامی است" })}
+        type="date"
         error={errors.date}
-        className='w-full'
+        className="w-full"
       />
 
       <Input
-        label='توضیحات'
-        {...register("description", {required: "توضیحات الزامی است"})}
+        label="توضیحات"
+        {...register("description", { required: "توضیحات الزامی است" })}
         error={errors.description}
-        className='w-full'
-        placeholder='توضیحات را وارد کنید'
+        className="w-full"
+        placeholder="توضیحات را وارد کنید"
       />
-   <Button type='submit' variant='primary' className='w-full'>
-    {defaultValues ? "ویرایش هزینه" : "افزودن هزینه"}
-   </Button>
+      <Button type="submit" variant="primary" className="w-full">
+        {defaultValues ? "ویرایش هزینه" : "افزودن هزینه"}
+      </Button>
     </form>
-  )
-}
+  );
+};
 
-export default ExpenseForm
+export default ExpenseForm;
