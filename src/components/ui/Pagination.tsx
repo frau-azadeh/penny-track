2f0e7a6e4364f6160d8c914d00962d4e53160774
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-04-21 16:42:50 +0330

    update Pagination

--- src/components/ui/Pagination.tsx
+++ src/components/ui/Pagination.tsx
@@ -1,30 +1,30 @@
-import React from "react";
-import Button from "./Button";
+import React from 'react'
+import Button from './Button';
 
-interface PaginationProps {
+interface PaginationProps{
   currentPage: number;
-  totalPages: number;
+  totalPage: number;
   onPageChange: (page: number) => void;
 }
 
 const Pagination: React.FC<PaginationProps> = ({
   currentPage,
-  totalPages,
+  totalPage,
   onPageChange,
 }) => {
   return (
     <div>
-      {Array.from({ length: totalPages }).map((_, index) => (
+      {Array.from({length: totalPage}).map((_, index)=>(
         <Button
           key={index}
-          variant={index + 1 === currentPage ? "primary" : "secondary"}
-          onClick={() => onPageChange(index + 1)}
+          variant={index+1 === currentPage ? "primary" : "secondary"}
+          onClick={() => onPageChange(index +1)}
         >
-          {index + 1}
+          {index+1}
         </Button>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default Pagination;
+export default Pagination
